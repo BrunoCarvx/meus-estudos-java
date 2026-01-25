0ab2273ba6c330a2d8dbe464907a235a0a4ed608
BrunoCarvx <carvalhodeassisbruno@gmail.com>
2026-01-25 16:34:08 -0300

    Update EncontradorDeFrutas.java

--- EncontradorDeFrutas.java
+++ EncontradorDeFrutas.java
@@ -4,7 +4,27 @@ public class EncontradorDeFrutas{
     public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
 
-        //variável
-        String[] frutas = 
+        //variáveis
+        String[] frutas = {"Banana", "Uva", "Maçã", "Pêra", "Abacaxi"};
+        boolean encontrei = false;
+
+        System.out.print("Diga uma fruta para eu ver se eu tenho (digite corretamente): ");
+        String frutaDigitada = leitor.nextLine();
+
+        leitor.close();
+
+        for (int i = 0; i < 5; i++){
+            if (frutas[i].equals(frutaDigitada)){
+                encontrei = true;
+                break;
+            }
+        }
+
+        if (encontrei){
+            System.out.println("Encontrei!");
+        }
+        else{
+            System.out.println("Não encontrei :(");
+        }
     }
 }
